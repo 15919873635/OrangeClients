34300cfda246daf2b18d92069babde52923bc4e3
15919873635 <15919873635@126.com>
2017-06-26 10:04:56 +0800

    新增主函数日志

--- src/com/orange/clients/MainClass.java
+++ src/com/orange/clients/MainClass.java
@@ -1,15 +1,19 @@
 package com.orange.clients;
 
+import org.apache.log4j.Logger;
+
 import com.orange.clients.ui.MainWindow;
 
 public class MainClass {
 
+	public static Logger logger = Logger.getLogger(MainClass.class);
+	
 	public static void main(String[] args) {
 		try {
 			MainWindow window = new MainWindow();
 			window.open();
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error(e.getMessage());
 		}
 	}
 }
